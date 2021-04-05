23ccd04fec0540c11dad92079cf77e0e538d3583
Gabe Ortiz <gabe@signalnine.net>
2021-04-05 13:44:05 -0600

    initial commit

---
+++ check_bday.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import datetime
+import tweepy
+import os
+import requests
+from dotenv import load_dotenv
+from instadm import InstaDM
+
+tweet = ""
+insta = ""
+df = pd.read_csv("/home/gabe/birthdaybot/birthdays.csv")
+today = datetime.datetime.now().strftime("%m-%d")
+for index, item in df.iterrows():
+    bday = item['Birthday']
+    if(bday == today and item['Twitter'] != 'na'):
+        print(item['Name'] + "'s birthday")
+        tweet = (item['Twitter'] + " happy birthday!")
+    elif(bday == today and item['Instagram'] != 'na'):
+        insta = item['Instagram']
+    elif(bday == today):
+        resp = requests.post('https://textbelt.com/text', {
+            'phone': '4159525775',
+            'message': item['Name'] + '\'s birthday',
+            'key': os.getenv("TBKEY",
+        })
+        print(resp.json())
+if (tweet != ""):
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(os.getenv("APIKEY"), os.getenv("APISECRET"))
+    auth.set_access_token(os.getenv("TOKEN"), os.getenv("TOKENSECRET"))
+    api = tweepy.API(auth)
+    # tweet
+    api.update_status(tweet)
+    print(tweet)
+
+if (insta != ""):
+    # instagram auth
+    instamsg = InstaDM(username=os.getenv("INSTAUSER"),
+                       password=os.getenv("INSTAPASS"), headless=True)
+    # dm user
+    instamsg.sendMessage(user=insta, message='happy birthday!')
+    print(insta)
